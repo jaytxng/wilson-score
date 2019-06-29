894727525225de6916c49addbb2c3d8194d3fbd6
Jay Tong <jayktong@gmail.com>
2019-06-29 00:36:11 -0700

    Complete pnorm and rating interval tests

--- wilsonscore.test.js
+++ wilsonscore.test.js
@@ -59,6 +59,52 @@ test('.lowerBound() invokes .interval() function', () => {
   spy.mockRestore();
 });
 
+test('.ratingInterval() is accurate to 4 digits after the decimal with confidence and correction explicitly set', () => {
+  const result = WilsonScore.ratingInterval(2.5, 2, 1, 5, { confidence: 0.95, correction: true });
+  result.left = parseFloat(result.left.toFixed(4));
+  result.right = parseFloat(result.right.toFixed(4));
+  const left = parseFloat((1 + 4 * 0.007269).toFixed(4));
+  const right = parseFloat((1 + 4 * 0.9439).toFixed(4));
+  const expected = { left, right };
+  expect(result).toStrictEqual(expected);
+});
+
+test('.ratingInterval() defaults to confidence interval of 0.95 and correction enabled', () => {
+  const resultWithoutOptions = WilsonScore.ratingInterval(2.5, 2, 1, 5);
+  const options = { confidence: 0.95, correction: true };
+  const resultWithOptions = WilsonScore.ratingInterval(2.5, 2, 1, 5, options);
+  expect(resultWithoutOptions).toStrictEqual(resultWithOptions);
+});
+
+test('.ratingInterval() set to non-default confidence interval % is accurate', () => {
+  const result = WilsonScore.ratingInterval(2.5, 2, 1, 5, { confidence: 0.90, correction: true });
+  result.left = parseFloat(result.left.toFixed(4));
+  result.right = parseFloat(result.right.toFixed(4));
+  const left = parseFloat((1 + 4 * 0.0099).toFixed(4));
+  const right = parseFloat((1 + 4 * 0.9272).toFixed(4));
+  const expected = { left, right };
+  expect(result).toStrictEqual(expected);
+});
+
+test('.ratingInterval() with correction disabled returns accurate result', () => {
+  const result = WilsonScore.ratingInterval(2.5, 2, 1, 5, { confidence: 0.95, correction: false });
+  result.left = parseFloat(result.left.toFixed(4));
+  result.right = parseFloat(result.right.toFixed(4));
+  const left = parseFloat((1 + 4 * 0.0561).toFixed(4));
+  const right = parseFloat((1 + 4 * 0.8583).toFixed(4));
+  const expected = { left, right };
+  expect(result).toStrictEqual(expected);
+});
+
+test('.ratingInterval() invokes .interval() function', () => {
+  const spy = jest.spyOn(WilsonScore, 'interval');
+  WilsonScore.ratingInterval(2.5, 2, 1, 5, { confidence: 0.90, correction: true });
+
+  expect(spy).toHaveBeenCalled();
+
+  spy.mockRestore();
+});
+
 test('._pnorm() throws error if input is out of bounds', () => {
   const forceLessThanZero = () => WilsonScore._pnorm(-1);
   const forceGreaterThanOne = () => WilsonScore._pnorm(2);
